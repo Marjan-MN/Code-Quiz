042b18ec515836e66a8a53d3ebfc2756a8bbb5c1
Marjan-MN <131532495+Marjan-MN@users.noreply.github.com>
2023-05-20 22:54:48 -0500

    updated quiz questions js

---
+++ assets/JS/quiz-questions.js
@@ -0,0 +1,35 @@
+var questions = [
+
+    {
+        question: "Commonly used data types DO NOT include."
+        options: ['Strings', 'Booleans','Alerts', 'integers']
+        answer: 'Alerts'
+    },
+
+    {
+        question: "The condition in an if/else statement is enclosed within_____."
+        options: ['Quots', 'Curly bracket','Parenthesis', 'Square bracket']
+        answer: 'Curly bracket'
+    },
+
+    {
+        question: "Arrays in javaScripts can be used to store______."
+        options: ['Numbers and strings', 'Other Arrays','Booleans', 'All of the above']
+        answer: 'All of the above'
+    },
+    {
+        question: "String values must be enclosed within______ when being assigned to variables."
+        options: ['Commas', 'Curly brackets','Quots', 'Parenthesis']
+        answer: 'Quots'
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:."
+        options: ['JavaScriptes', 'Terminal/Bash','for loop', 'Console.log']
+        answer: 'Console.log'
+    },
+
+]
+
+questions[currentQuestion].question
+questions[currentQuestion].options
+questions[currentQuestion].answer
